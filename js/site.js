ffd69e241f8e6f275a9ffaf387d133a934a1d456
Kingcean Tuan <kingcean@live.com>
2021-10-03 11:21:00 +0800

    Update references.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-﻿var site = {};
+var site = {};
 (function(site) {
 
     site.query = function (name) {
@@ -8,21 +8,16 @@
         try {
             if (typeof name === "string") {
                 var result = url.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
-                if (result == null || result.length < 1) {
-                    return "";
-                }
+                if (result == null || result.length < 1) return "";
                 return notToDecode ? result[1] : decodeURIComponent(result[1]);
-            }
-            else if (typeof name === "number") {
+            } else if (typeof name === "number") {
                 var result = url.match(new RegExp("[\?\&][^\?\&]+=[^\?\&]+", "g"));
-                if (result == null) {
-                    return "";
-                }
+                if (result == null) return "";
                 return notToDecode ? result[name].substring(1) : decodeURIComponent(result[name].substring(1));
             }
         }
         catch (ex) { }
-            return null;
+        return null;
     };
 
     site.firstQuery = function () {
@@ -41,13 +36,8 @@
     site.head = function () {
         var cntEle = document.createElement("header");
         cntEle.id = "page_head";
-        cntEle.innerHTML = '<section><h1><a href="http://www.kingcean.com/">Kingcean</a><span> &gt; </span><a href="http://github.kingcean.com/">GitHub</a></h1></section>';
+        cntEle.innerHTML = '<section><h1><a href="http://kingcean.net/">Kingcean</a></h1><ul><li><a href="https://github.com/kingcean?tab=repositories">Repositories</a></li><li><a href="http://kingcean.net/blog">.NET Blogs</a></li><li><a href="http://kingcean.org/blog">Front-end Blogs</a></li></ul></section>';
         document.body.appendChild(cntEle);
-
-        var cnt2Ele = document.createElement("header");
-        cnt2Ele.id = "page_menu";
-        cnt2Ele.innerHTML = '<section><ul><li><a href="http://github.kingcean.com/">Home</a></li><li><a href="https://github.com/kingcean?tab=repositories">Repositories</a></li><li><a href="http://github.kingcean.com/blog/dotnet">.Net Dev Blogs</a></li><li><a href="http://github.kingcean.com/blog/web">Web Dev Blogs</a></li><li><a href="http://github.kingcean.com/blog/qianduan">前端开发博客</a></li><li><a href="http://github.kingcean.com/blog/dian-net">.Net 开发博客</a></li></ul></section>';
-        document.body.appendChild(cnt2Ele);
     };
 
     site.blogs = function () {
@@ -92,13 +82,16 @@
                 articleStr = "<h1>" + item.name + "</h1><section><em>Loading...</em></section>";
                 r.list.some(function (item) {
                     if (!item || item.invalid || item.id !== id) return false;
-                    $.get("/blog" + item.url).then(function (r2) {
+                    var relaPath = "/blog";
+                    try {
+                        if (location.pathname.endsWith("/blog/")) relaPath = ".";
+                    } catch (ex) {}
+                    $.get(relaPath + item.url).then(function (r2) {
                         var md = new Remarkable();
-                        r2 = r2.replace(/\(.\//g, "(/blog/" + item.dir + "/");
+                        r2 = r2.replace(/\(.\//g, "(" + relaPath + "/" + item.dir + "/");
                         cntEle.innerHTML = "<h1>" + item.name + "</h1><section>" + md.render(r2) + "</section>" + cntStr;
                     }, function (r) {
-                        cntEle.innerHTML = "<h1>" + item.name + "</h1><section><em>Failed to load.</em></section>" + cntStr;
-
+                        cntEle.innerHTML = "<h1>" + item.name + "</h1><section><em>Load failed.</em></section>" + cntStr;
                     });
                     return true;
                 });
@@ -112,7 +105,7 @@
             cntStr += "</ul>";
             cntEle.innerHTML = articleStr + cntStr;
         }, function (r) {
-            cntEle.innerHTML = "<em>Failed to load.</em>";
+            cntEle.innerHTML = "<em>Load failed.</em>";
         });
     };
     
